d5f4686a8d8a656c8faccefd6bdaff8a87be81da
parth.vijayvargiya@lrz.de <parth.vijayvargiya@lrz.de>
2025-05-08 09:13:25 +0200

    testing

---
+++ src/components/Pages/FAQ/FAQContent.js
@@ -0,0 +1,15 @@
+import { useSelector } from "react-redux";
+import BlankCard from "../../UI/Card/BlankCard";
+import { Fragment } from "react";
+
+const FAQContent = () => {
+    const darkmode = useSelector((state) => state.accessibilities.darkmode);
+    const fs = useSelector((state) => state.accessibilities.font_size);
+    const page_header_fs = +fs * 1.5;
+    return (
+        <Fragment>
+        </Fragment>
+    );
+};
+
+export default FAQContent;
