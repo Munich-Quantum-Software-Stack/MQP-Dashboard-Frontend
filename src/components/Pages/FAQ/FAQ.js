26a70a3770eef4b7b68cef655588983218416c25
parth.vijayvargiya@lrz.de <parth.vijayvargiya@lrz.de>
2025-05-08 09:08:21 +0200

    testing

---
+++ src/components/Pages/FAQ/FAQ.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import ContentCard from "../../UI/Card/ContentCard";
+import FAQContent from "./FAQContent";
+
+import "./FAQ.scss";
+
+function FAQ() {
+    const darkmode = useSelector((state) => state.accessibilities.darkmode);
+    const fs = useSelector((state) => state.accessibilities.font_size);
+    const page_header_fs = +fs * 1.5;
+    return (
+        <React.Fragment>
+            <ContentCard className={`faq_container ${darkmode ? "dark_bg" : "white_bg"} h-100`}>
+                <h4
+                    className="page_header"
+                    style={{ fontSize: page_header_fs }}
+                >
+                    FAQs will be displayed shortly. Please contact us via email if you need anything for now.
+                </h4>
+            </ContentCard>
+        </React.Fragment>
+    );
+}
+
+export default Information;
