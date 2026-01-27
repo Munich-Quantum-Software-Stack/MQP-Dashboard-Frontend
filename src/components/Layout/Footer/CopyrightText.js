ef0995a56c25eec6c6a6781601d2c8c3bc4df076
Huynh <di76xes@badwlrz-cm25806.ws.lrz.de>
2026-01-27 14:17:44 +0100

    Add LRZ logo, MQV and MQSS logos on Login page, update copyright text

--- src/components/Layout/Footer/CopyrightText.js
+++ src/components/Layout/Footer/CopyrightText.js
@@ -4,8 +4,8 @@ const CopyrightText = () => {
     <div>
       <div className="my-2 copyright_text ">
         <div className="mb-2 text-center">
-          &copy; {current_year} Powered by the Munich Quantum Portal (MQP),&nbsp;{' '}
-          <a href="https://github.com/Munich-Quantum-Software-Stack">open-source software</a>{' '}
+          &copy; {current_year} Powered by the Munich Quantum Portal (MQP),{' '}
+          <a href="https://github.com/Munich-Quantum-Software-Stack" target="_blank" rel="noopener noreferrer">open-source software</a>{' '}
           developed by the Munich Quantum Software Stack (MQSS) Team
         </div>
       </div>
--- src/components/Layout/Footer/FooterLogos.js
+++ src/components/Layout/Footer/FooterLogos.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import MQSSLogo from '@assets/images/logo-mqss-light.svg';
-import MQVLogo from '@assets/images/MQV_LOGO_F_BUNT.png';
 
 function FooterLogos() {
   return (
@@ -11,10 +9,22 @@ function FooterLogos() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src={MQSSLogo} className="footer_logo_img" alt="Munich Quantum Software Stack" />
+          <img
+            src="/images/logo-mqss-light.svg"
+            className="footer_logo_img"
+            alt="Munich Quantum Software Stack"
+          />
         </a>
-        <a href="https://www.munich-quantum-valley.de" target="_blank" rel="noopener noreferrer">
-          <img src={MQVLogo} className="footer_logo_img" alt="Munich Quantum Valley" />
+        <a
+          href="https://www.munich-quantum-valley.de"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/images/MQV_Logo_Blue.svg"
+            className="footer_logo_img"
+            alt="Munich Quantum Valley"
+          />
         </a>
       </div>
     </div>
