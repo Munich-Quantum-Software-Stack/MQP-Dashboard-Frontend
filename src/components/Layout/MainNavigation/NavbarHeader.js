c1080d0c086b663a76a9616c246768bb8d6875ae
Huynh <di76xes@badwlrz-cm25806.fritz.box>
2026-01-30 13:11:03 +0100

    feat: replace logo on topbar

--- src/components/Layout/MainNavigation/NavbarHeader.js
+++ src/components/Layout/MainNavigation/NavbarHeader.js
@@ -4,14 +4,14 @@ import { motion } from 'framer-motion';
 /** Animated MQP logo header */
 const NavbarHeader = () => {
   return (
-    <div className="topbar_mqp_logo_wrap" title="Munich Quantum Portal">
+    <div className="topbar_logo_wrap" title="Munich Quantum Portal">
       <motion.div
-        className="mqp_logo"
+        className="topbar_logo"
         initial={{ opacity: 0, width: 0 }}
         animate={{ opacity: 1, width: 'auto' }}
         transition={{ duration: 0.2 }}
       >
-        <img src="/images/MQV_Logo_Blue.svg" className="mqp_logo_img" alt="MQP logo" />
+        <img src="/images/lrz_wortbild_square.png" className="topbar_logo_img" alt="LRZ" />
         <span className="logo_text"></span>
       </motion.div>
     </div>
