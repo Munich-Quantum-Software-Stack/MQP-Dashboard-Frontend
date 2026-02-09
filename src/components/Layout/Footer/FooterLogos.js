cc17b7fd5ae5fee884e78fb4ea71d4433882b647
Trang Huynh <Trang.Huynh@lrz.de>
2026-02-09 14:45:01 +0100

    feat: continue keeping environment filesin order to not make confused about script's commands

--- src/components/Layout/Footer/FooterLogos.js
+++ src/components/Layout/Footer/FooterLogos.js
@@ -1,6 +1,6 @@
 import React from 'react';
-//import MQVLogo from "@assets/images/MQV_Logo_Blue.svg";
-//import MQSSLogo from "@assets/images/logo-mqss-light.svg";
+import MQSSLogo from '@assets/images/logo-mqss-light.svg';
+import MQVLogo from '@assets/images/MQV_LOGO_F_BUNT.png';
 
 function FooterLogos() {
   return (
@@ -11,18 +11,10 @@ function FooterLogos() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img
-            src="/images/logo-mqss-light.svg"
-            className="footer_logo_img"
-            alt="Munich Quantum Software Stack"
-          />
+          <img src={MQSSLogo} className="footer_logo_img" alt="Munich Quantum Software Stack" />
         </a>
         <a href="https://www.munich-quantum-valley.de" target="_blank" rel="noopener noreferrer">
-          <img
-            src="/images/MQV_LOGO_F_BUNT.png"
-            className="footer_logo_img"
-            alt="Munich Quantum Valley"
-          />
+          <img src={MQVLogo} className="footer_logo_img" alt="Munich Quantum Valley" />
         </a>
       </div>
     </div>
